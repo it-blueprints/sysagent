031b651d85edb28c04cc044c59a92bdb3d44adea
Nilanjan Biswas <nilanjan.biswas@dwp.gov.uk>
2024-01-23 17:39:39 +0000

    X

--- src/main/java/com/itblueprints/sysagent/SysAgentService.java
+++ src/main/java/com/itblueprints/sysagent/SysAgentService.java
@@ -1,29 +1,27 @@
 package com.itblueprints.sysagent;
 
-import com.itblueprints.sysagent.cluster.ClusterService;
 import com.itblueprints.sysagent.cluster.NodeState;
 import com.itblueprints.sysagent.job.JobRecord;
 import com.itblueprints.sysagent.job.JobService;
 import com.itblueprints.sysagent.scheduling.JobScheduleRecord;
 import com.itblueprints.sysagent.step.StepRecord;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class SysAgentService {
 
-    private final MongoTemplate mongoTemplate;
+    private final MongoOperations mongoOperations;
     private final JobService jobService;
 
     //--------------------------------
     public void deleteClusterData() {
-        mongoTemplate.remove(new Query(), JobRecord.class);
-        mongoTemplate.remove(new Query(), JobScheduleRecord.class);
-        mongoTemplate.remove(new Query(), NodeState.class);
-        mongoTemplate.remove(new Query(), StepRecord.class);
+        mongoOperations.dropCollection(JobRecord.class);
+        mongoOperations.dropCollection(JobScheduleRecord.class);
+        mongoOperations.dropCollection(NodeState.class);
+        mongoOperations.dropCollection(StepRecord.class);
     }
 
     //--------------------------------
