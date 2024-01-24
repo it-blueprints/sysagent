f7ef398d3a22347e9db77e313067888ecaad9d7d
Nilanjan Biswas <nilanjan.biswas@dwp.gov.uk>
2024-01-24 15:56:06 +0000

    X

--- src/main/java/com/itblueprints/sysagent/step/StepContext.java
+++ src/main/java/com/itblueprints/sysagent/step/StepContext.java
@@ -5,24 +5,23 @@ import lombok.Getter;
 import lombok.Setter;
 
 @Getter
-@Setter
 public class StepContext {
 
     private Arguments arguments = new Arguments();
 
-    private StepData stepData;
-
-    private CheckPointState checkPointState;
-
     private Integer partitionNum;
+    void setPartitionNum(Integer partitionNum) {this.partitionNum = partitionNum;}
 
     private Integer totalPartitions;
+    void setTotalPartitions(Integer totalPartitions) {this.totalPartitions = totalPartitions;}
 
     private long itemsProcessed;
     void setItemsProcessed(long itemsProcessed) {
         this.itemsProcessed = itemsProcessed;
     }
 
+    @Setter
+    private CheckPointState checkPointState;
 
 
 }
