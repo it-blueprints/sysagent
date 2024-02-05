cf702f399712eac4848743944b40cb781dbafca3
Nilanjan Biswas <nilanjan.biswas@dwp.gov.uk>
2024-02-05 10:58:07 +0000

    X

--- src/main/java/com/itblueprints/sysagent/job/PipelineStep.java
+++ src/main/java/com/itblueprints/sysagent/job/PipelineStep.java
@@ -2,7 +2,7 @@ package com.itblueprints.sysagent.job;
 
 import com.itblueprints.sysagent.step.Step;
 
-class PipelineStep {
+public class PipelineStep {
     public Step step;
     public String onOutcome;
     public PipelineStep nextPipelineStep;
