a4026601e99a1a48f6e31976ed2d7bdc027fffd5
Nilanjan Biswas <nilanjan.biswas@gmail.com>
2024-02-09 00:17:13 +0000

    Added clearAll() to RecordRepository and refactored SysAgentService.resetCluster()

--- src/main/java/com/itblueprints/sysagent/SysAgentService.java
+++ src/main/java/com/itblueprints/sysagent/SysAgentService.java
@@ -3,10 +3,11 @@ package com.itblueprints.sysagent;
 import com.itblueprints.sysagent.cluster.NodeRecord;
 import com.itblueprints.sysagent.job.JobRecord;
 import com.itblueprints.sysagent.job.JobExecutionService;
+import com.itblueprints.sysagent.repository.RecordRepository;
 import com.itblueprints.sysagent.scheduling.JobScheduleRecord;
 import com.itblueprints.sysagent.step.StepRecord;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
@@ -15,15 +16,12 @@ import java.time.LocalDateTime;
 @RequiredArgsConstructor
 public class SysAgentService {
 
-    private final MongoOperations mongoOperations;
+    private final RecordRepository repository;
     private final JobExecutionService jobExecutionService;
 
     //--------------------------------
     public void resetCluster() {
-        mongoOperations.dropCollection(JobRecord.class);
-        mongoOperations.dropCollection(JobScheduleRecord.class);
-        mongoOperations.dropCollection(NodeRecord.class);
-        mongoOperations.dropCollection(StepRecord.class);
+        repository.clearAll();
     }
 
     //--------------------------------
