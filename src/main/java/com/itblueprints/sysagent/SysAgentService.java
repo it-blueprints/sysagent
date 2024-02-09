b7147731a6d750c310bed39eba5c1fb6c07b51cf
Nilanjan Biswas <nilanjan.biswas@gmail.com>
2024-02-09 00:17:42 +0000

    Updated imports

--- src/main/java/com/itblueprints/sysagent/SysAgentService.java
+++ src/main/java/com/itblueprints/sysagent/SysAgentService.java
@@ -1,13 +1,8 @@
 package com.itblueprints.sysagent;
 
-import com.itblueprints.sysagent.cluster.NodeRecord;
-import com.itblueprints.sysagent.job.JobRecord;
 import com.itblueprints.sysagent.job.JobExecutionService;
 import com.itblueprints.sysagent.repository.RecordRepository;
-import com.itblueprints.sysagent.scheduling.JobScheduleRecord;
-import com.itblueprints.sysagent.step.StepRecord;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
